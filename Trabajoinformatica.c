8ffa83f67c2a21722accb5e894e8646d2c864b55
gmalvarez <37289752+gmalvarez@users.noreply.github.com>
2019-05-21 19:20:42 +0200

    Update Trabajoinformatica.c

--- Trabajoinformatica.c
+++ Trabajoinformatica.c
@@ -1,2 +1,11 @@
 //TRABAJO INFORMATICA 2018-2019
+#include<stdio.h>
+#include<string.h>
 
+typedef struct{
+	char titulo[50];
+	char autor[50];
+	char genero[50];
+	int year;
+	int estado; //1=nuevo, 2=usado, 3=deteriorado
+}libro; // estructura de cada libro
