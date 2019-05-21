4255667a4703a4eac59d5b3555aa4ee125255635
alvaromitachungara <36919700+alvaromitachungara@users.noreply.github.com>
2019-05-21 19:50:09 +0200

    TRABAJO
    funciones de buscar

--- Trabajoinformatica.c
+++ Trabajoinformatica.c
@@ -31,6 +31,135 @@ typedef struct{
 
 
 //funciones
+int buscaautor(){
+	int i=0,j=0,k=0,tmp1,tmp2;// para el bucle y para comparar
+	int N=devuelveN( );//para saber el numero de peliculas
+	char autor[50];
+	libro lib[N];
+	libro aux;
+	FILE *pbiblioteca;
+    pbiblioteca=fopen("Biblioteca.txt","r");	
+
+	
+	if (pbiblioteca == NULL)
+	{
+		printf("Error al abrir el fichero.\n");
+		return -1;
+	}
+	else
+	{
+		fflush(stdin); 	
+    	printf("\nIntroduzca el autor del libro: \n");
+		scanf("%[^\n]",autor);
+		
+		while (feof(pbiblioteca) == 0) //Primero leemos los datos
+		{
+			fscanf(pbiblioteca, "%[^;]; %[^;]; %[^;]; %i; %d;", lib[i].titulo, lib[i].autor, lib[i].genero, &lib[i].year, &lib[i].estado);
+			i++;
+		}
+		
+		printf("\nTitulo\t\t\t\t\tAutor\t\t\tGenero\t\tA%co\t\tEstado\n\n",164);
+	
+		for(i=0; i<=N-1; i++) //Lo ordenamos
+		{
+			for(j=i+1; j<=N-1 ; j++) // Comparamos cada elemento con el siguiente
+			{
+				if (strcmp(lib[i].autor,lib[j].autor)>0)//Si es mayor(alfabeticamente) intercambiamos el contenido de los dos elementos
+				{
+				aux = lib[i]; 
+				lib[i] = lib[j];
+				lib[j] = aux;
+				}
+			}
+			
+			for(k=0; k<=strlen(lib[i].autor); k++)//bucle para recorrer cada pelicula
+			{
+				if (autor[0]==lib[i].autor[k])//comprobamos que coincidan el titulo introducido y el titulo de la pelicula registrada
+				{
+					tmp1=0;
+					tmp2=k;
+					
+					while ((autor[tmp1]==lib[i].autor[tmp2]) && (tmp2<strlen(lib[i].autor)) && (tmp1!=strlen(autor)))
+					{
+                		tmp1++;
+						tmp2++;
+						if (tmp1==strlen(autor))
+                    	{
+						printf("%-40s %-23s %-15s %i\t\t%d\n",lib[i].titulo, lib[i].autor, lib[i].genero, lib[i].year, lib[i].estado);
+                  		}
+	              	}
+    	        } 
+        	}
+			
+		}	
+		printf("\n");
+	}
+	return 0;
+}
+int buscalibro( )
+{
+	int i=0,j=0,k=0,tmp1,tmp2;// para el bucle y para comparar
+	int N=devuelveN( );//para saber el numero de peliculas
+	char titulo[50];
+	libro lib[N];
+	libro aux;
+	FILE *pbiblioteca;
+    pbiblioteca=fopen("Biblioteca.txt","r");	
+
+	
+	if (pbiblioteca == NULL)
+	{
+		printf("Error al abrir el fichero.\n");
+		return -1;
+	}
+	else
+	{
+		fflush(stdin); 	
+    	printf("\nIntroduzca el titulo del libro: \n");
+		scanf("%[^\n]",titulo);
+		
+		while (feof(pbiblioteca) == 0) //Primero leemos los datos
+		{
+			fscanf(pbiblioteca, "%[^;]; %[^;]; %[^;]; %i; %d;", lib[i].titulo, lib[i].autor, lib[i].genero, &lib[i].year, &lib[i].estado);
+			i++;
+		}
+		
+		printf("\nTitulo\t\t\t\t\tAutor\t\t\tGenero\t\tA%co\t\tEstado\n\n",164);
+	
+		for(i=0; i<=N-1; i++) //Lo ordenamos
+		{
+			for(j=i+1; j<=N-1 ; j++) // Comparamos cada elemento con el siguiente
+			{
+				if (strcmp(lib[i].titulo,lib[j].titulo)>0)//Si es mayor(alfabeticamente) intercambiamos el contenido de los dos elementos
+				{
+				aux = lib[i]; 
+				lib[i] = lib[j];
+				lib[j] = aux;
+				}
+			}
+			
+			for(k=0; k<=strlen(lib[i].titulo); k++)//bucle para recorrer cada libro
+			{
+				if (titulo[0]==lib[i].titulo[k]){//comprobamos que coincidan el titulo introducido y el titulo del libro registrado
+					tmp1=0;
+					tmp2=k;
+					
+					while ((titulo[tmp1]==lib[i].titulo[tmp2]) && (tmp2<strlen(lib[i].titulo)) && (tmp1!=strlen(titulo)))
+					{
+                		tmp1++;
+						tmp2++;
+						if (tmp1==strlen(titulo))
+                    		printf("%-40s %-23s %-15s %i\t\t%d\n",lib[i].titulo, lib[i].autor, lib[i].genero, lib[i].year, lib[i].estado);
+	              	}
+    	        }
+        	}	
+		
+		}	
+		printf("\n");
+	}
+	return 0;
+}
+
 int buscaryear()
 {
 	int i=0,existe=0;// para el bucle y para usar en el if y saber si hay pelis con el año dado
@@ -72,5 +201,69 @@ int buscaryear()
 	}
 	return 0;
 }			
-    }   
-    
+//funcion para buscar por genero
+int buscagenero(){
+	int i=0,j=0,k=0,tmp1,tmp2;// para el bucle y para comparar
+	int N=devuelveN( );//para saber el numero de peliculas
+	char genero[50];
+	libro lib[N];
+	libro aux;
+	FILE *pbiblioteca;
+    pbiblioteca=fopen("Biblioteca.txt","r");	
+
+	
+	if (pbiblioteca == NULL)
+	{
+		printf("Error al abrir el fichero.\n");
+		return -1;
+	}
+	else
+	{
+		fflush(stdin); 	
+    	printf("\nIntroduzca el genero del libro: \n");
+		scanf("%[^\n]",genero);
+		
+		while (feof(pbiblioteca) == 0) //Primero leemos los datos
+		{
+			fscanf(pbiblioteca, "%[^;]; %[^;]; %[^;]; %i; %d;", lib[i].titulo, lib[i].autor, lib[i].genero, &lib[i].year, &lib[i].estado);
+			i++;
+		}
+		
+		printf("\nTitulo\t\t\t\t\tAutor\t\t\tGenero\t\tA%co\t\tEstado\n\n",164);
+	
+		for(i=0; i<=N-1; i++) //Lo ordenamos
+		{
+			for(j=i+1; j<=N-1 ; j++) // Comparamos cada elemento con el siguiente
+			{
+				if (strcmp(lib[i].genero,lib[j].genero)>0)//Si es mayor(alfabeticamente) intercambiamos el contenido de los dos elementos
+				{
+				aux = lib[i]; 
+				lib[i] = lib[j];
+				lib[j] = aux;
+				}
+			}
+			
+			for(k=0; k<=strlen(lib[i].genero); k++)//bucle para recorrer cada pelicula
+			{
+				if (genero[0]==lib[i].genero[k])//comprobamos que coincidan el titulo introducido y el titulo de la pelicula registrada
+				{
+					tmp1=0;
+					tmp2=k;
+					
+					while ((genero[tmp1]==lib[i].genero[tmp2]) && (tmp2<strlen(lib[i].genero)) && (tmp1!=strlen(genero)))
+					{
+                		tmp1++;
+						tmp2++;
+						if (tmp1==strlen(genero))
+                    	{
+						printf("%-40s %-23s %-15s %i\t\t%d\n",lib[i].titulo, lib[i].autor, lib[i].genero, lib[i].year, lib[i].estado);
+                  		}
+	              	}
+    	        } 
+        	}
+			
+		}	
+		printf("\n");
+	}
+	return 0;
+}
